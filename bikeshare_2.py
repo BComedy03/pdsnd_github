9ff311f80663a1eaa1726fa09e9f79d70540722e
BComedy03 <yaojia571@hotmail.com>
2021-08-01 22:49:48 +0200

    Refactoring and ask name for user

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -16,7 +16,8 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    name = input("\n What is your name?\n")
+    print('Hello {}! Let\'s explore some US bikeshare data!', format(name))
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
         city = input("\nWhich city would you like to filter by? New York City, Chicago or Washington? \n").lower()
